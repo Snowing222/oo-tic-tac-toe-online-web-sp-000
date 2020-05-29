c8ac0056fbbacd6e5bc0c2761e616f6ccdd7c21c
elsa chen <snowing222@gmail.com>
2020-05-29 01:46:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,6 +45,22 @@ class TicTacToe
       turn
     end
   end
+#WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  def won?
+    WIN_COMBINATIONS.find do |x|
+      @board[0]==@board[1]&&@board[1]==@board[2]
+    end
+  end
+
+  def full?
+    @board.all? {|x| x!=" "}
+  end
+
+  
+        
+        
+      
+  end
 
 
 
