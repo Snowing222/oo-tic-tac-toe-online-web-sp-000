400023cc76ace7adefebef23658ae7936845d718
elsa chen <snowing222@gmail.com>
2020-05-29 01:36:42 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,52 @@
+class TicTacToe
+  WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  def initialize
+    @board=[" "," "," "," "," "," "," "," "," "]
+  end
+
+  def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(number)
+    index=number.to_i-1
+  end
+
+  def move(index,token="X")
+    @board[index]=token
+  end
+
+  def position_taken?(index)
+    @board[index]=="X"||@board[index]=="O"
+  end
+
+  def valid_move?(index)
+    !position_taken?(index)&&index<9&&index>=0
+  end
+
+  def turn_count
+    @board.count {|x| x!=" "}
+  end
+
+  def current_player
+    turn_count.even?? "X" : "O"
+  end
+
+  def turn
+    index=input_to_index(gets)
+    if valid_move?(index)
+      @board[index]=current_player
+      display_board
+    else
+      turn
+    end
+  end
+
+
+
+
+end
